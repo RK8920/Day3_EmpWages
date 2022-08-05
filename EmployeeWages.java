d619a40575ba43383505df8cf872baf278215793
Rohit Kamble <kamble.rohit10792@gmail.com>
2022-08-05 22:05:14 +0530

    Added: Functionality to check emp present or absent

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -4,7 +4,14 @@ public class EmployeeWages {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Employee Wages Program");
-    }
+        int FULL_TIME = 1;
+        int empCheck = (int) Math.floor(Math.random() * 10 % 2);
+        System.out.println(empCheck);
 
+        if (empCheck == FULL_TIME) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+    }
 }
